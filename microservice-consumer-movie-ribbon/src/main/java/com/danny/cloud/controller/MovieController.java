15afc6d0416f7992d740598da7956a9bd8fd8200
danny <447579568@qq.com>
2017-09-06 11:06:21 +0800

    update

--- microservice-consumer-movie-ribbon/src/main/java/com/danny/cloud/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/danny/cloud/controller/MovieController.java
@@ -3,12 +3,17 @@ package com.danny.cloud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.danny.cloud.entity.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @RestController
 public class MovieController {
@@ -18,10 +23,21 @@ public class MovieController {
   private LoadBalancerClient loadBalancerClient;
 
   @GetMapping("/movie/{id}")
-  public User findById(@PathVariable Long id) {
+  public User findById(@PathVariable Long id, HttpServletRequest req, HttpServletResponse resp) {
     // http://localhost:7900/simple/
     // VIP virtual IP
     // HAProxy Heartbeat
+    HttpHeaders headers = new HttpHeaders();
+    Enumeration<String> headerNames = req.getHeaderNames();
+    while (headerNames.hasMoreElements()) {
+      String key = (String) headerNames.nextElement();
+      String value = req.getHeader(key);
+      headers.add(key, value);
+    }
+//    String restStrResult = restTemplate.postForObject(url, /*携带header*/new HttpEntity<String>(headers), String.class,
+//            　　　　　　　　　　　　 /*携带parameter*/request.getParameterMap());
+
+    //目前没有发现发送携带header信息的getForObject方法
     return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
   }
 
