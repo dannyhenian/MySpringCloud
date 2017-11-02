21b9dc594ef1ea53253c911e1ebe203fbc24cbf1
Danny <3138001703@qq.com>
2017-11-02 17:08:03 +0800

    update

---
+++ springBoot-Mybatis/src/main/java/com/danny/cloud/core/task/AbstractTaskHandler.java
@@ -0,0 +1,14 @@
+package com.danny.cloud.core.task;/**
+ * Created by danny on 2017-09-06.
+ */
+
+/**
+ * 任务处理器
+ *
+ * @author danny
+ * @create 2017-09-06 18:01
+ */
+
+public abstract class AbstractTaskHandler {
+//    public abstract
+}
